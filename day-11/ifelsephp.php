27b5c2c024c34cbadd93be1a5de97733d3303979
vidhikharsani <137269666+vidhikharsani@users.noreply.github.com>
2023-07-06 11:43:27 +0530

    Add files via upload

---
+++ day-11/ifelsephp.php
@@ -0,0 +1,31 @@
+<?php 
+
+$x=9;
+$y=5;
+$z=4;
+$a=2;
+if($x<$y){
+    echo("x is less than y <br>");
+}
+else{
+    echo("x is not less than y <br>");
+}
+if($y<$z){
+    echo("y is less than z <br>");
+}
+else{
+    echo("y is not less than z <br>");
+}
+if($z<$a){
+    echo("z is less than a <br>" );
+}
+else{
+    echo("z is not less than a <br>");
+}
+if($a<$x){
+    echo("a is less than x <br>");
+}
+else{
+    echo("a is not less than x <br>");
+}
+?>
